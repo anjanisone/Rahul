c9331f2877c7d2f307e05eece9d6bcf383ca6963
anjanisone <emailing2prasanna@gmail.com>
2025-08-05 01:04:31 +0530

    Update main.tf

--- 4.8.25/modules/nat_gateway_association/main.tf
+++ 4.8.25/modules/nat_gateway_association/main.tf
@@ -1,30 +1,12 @@
-variable "nat_gateway_id" {
-  description = "ID of the NAT Gateway to associate"
-  type        = string
-}
-
-variable "subnets_to_associate" {
-  description = "List of existing subnets to associate with NAT Gateway"
-  type = list(object({
-    name                 = string
-    virtual_network_name = string
-    resource_group_name  = string
-  }))
-}
-
-data "azurerm_subnet" "existing_subnets" {
+resource "azurerm_subnet_nat_gateway_association" "assoc" {
   for_each = {
-    for subnet in var.subnets_to_associate : "${subnet.resource_group_name}-${subnet.virtual_network_name}-${subnet.name}" => subnet
+    for s in var.subnet_ids :
+    "${s.key}" => {
+      subnet_id      = s.value
+      nat_gateway_id = var.nat_gateway_id
+    }
   }
 
-  name                 = each.value.name
-  virtual_network_name = each.value.virtual_network_name
-  resource_group_name  = each.value.resource_group_name
-}
-
-resource "azurerm_subnet_nat_gateway_association" "associate_subnets" {
-  for_each = data.azurerm_subnet.existing_subnets
-
-  subnet_id      = each.value.id
-  nat_gateway_id = var.nat_gateway_id
+  subnet_id      = each.value.subnet_id
+  nat_gateway_id = each.value.nat_gateway_id
 }

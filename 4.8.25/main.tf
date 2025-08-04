809d1f277a3274496d279bac7769d31350335b50
anjanisone <emailing2prasanna@gmail.com>
2025-08-05 01:05:17 +0530

    Update main.tf

--- 4.8.25/main.tf
+++ 4.8.25/main.tf
@@ -1,39 +1,18 @@
-module "nat_gateway" {
-  source         = "../modules/nat_gateway"
-  name           = "my-nat"
-  location       = var.location
-  resource_group = var.resource_group
+module "nat_gateway_association_core" {
+  source          = "../modules/nat_gateway_association"
+  nat_gateway_id  = module.UE2-mgt-dev-nat-gw-core-build-agents.nat_gateway_id
+  subnet_ids      = {
+    build_agents = "/subscriptions/xxxx/resourceGroups/UE2-mgt-dev-core-build-agents/providers/Microsoft.Network/virtualNetworks/ue2-mgt-dev-core-build-agents/subnets/build-agents"
+    mdp_poc      = "/subscriptions/xxxx/resourceGroups/UE2-mgt-dev-core-build-agents/providers/Microsoft.Network/virtualNetworks/ue2-mgt-dev-core-build-agents/subnets/mdp-poc"
+  }
 }
 
-module "nat_gateway_association" {
-  source             = "../modules/nat_gateway_association"
-  nat_gateway_id     = module.nat_gateway.nat_gateway_id
-
-  subnets_to_associate = [
-    {
-      name                 = "build-agents"
-      virtual_network_name = "ue2-mgt-dev-core-build-agents"
-      resource_group_name  = "ue2-mgt-dev-core-build-agents"
-    },
-    {
-      name                 = "mdp-poc"
-      virtual_network_name = "ue2-mgt-dev-core-build-agents"
-      resource_group_name  = "ue2-mgt-dev-core-build-agents"
-    },
-    {
-      name                 = "impg-pe-sn"
-      virtual_network_name = "ue2-mgt-dev-psig-ml-vnet"
-      resource_group_name  = "ue2-mgt-dev-psig-ml"
-    },
-    {
-      name                 = "impg-aml-sn"
-      virtual_network_name = "ue2-mgt-dev-psig-ml-vnet"
-      resource_group_name  = "ue2-mgt-dev-psig-ml"
-    },
-    {
-      name                 = "AzureFirewallSubnet"
-      virtual_network_name = "ue2-mgt-dev-psig-ml-vnet"
-      resource_group_name  = "ue2-mgt-dev-psig-ml"
-    }
-  ]
+module "nat_gateway_association_psig" {
+  source          = "../modules/nat_gateway_association"
+  nat_gateway_id  = module.UE2-mgt-dev-nat-gw-psig-ml.nat_gateway_id
+  subnet_ids      = {
+    impg_pe_sn          = "/subscriptions/xxxx/resourceGroups/UE2-mgt-dev-psig-ml/providers/Microsoft.Network/virtualNetworks/ue2-mgt-dev-psig-ml/subnets/impg-pe-sn"
+    impg_aml_sn         = "/subscriptions/xxxx/resourceGroups/UE2-mgt-dev-psig-ml/providers/Microsoft.Network/virtualNetworks/ue2-mgt-dev-psig-ml/subnets/impg-aml-sn"
+    AzureFirewallSubnet = "/subscriptions/xxxx/resourceGroups/UE2-mgt-dev-psig-ml/providers/Microsoft.Network/virtualNetworks/ue2-mgt-dev-psig-ml/subnets/AzureFirewallSubnet"
+  }
 }
